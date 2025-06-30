1973cfa610d8dbdcfe78a3a66850ce6a9f4ab6b0
LUX14Zx <lux14zx.dev@gmail.com>
2025-06-30 13:58:45 +0700

    Update

---
+++ README.md
@@ -0,0 +1,13 @@
+Bonrecreme 
+
+
+
+
+Tech stack
+
+- vite
+- node.js
+- spring boot
+- postgres
+- redis
+- kafka
---
+++ docker/database/init_db.sql
@@ -0,0 +1,7 @@
+-- init.sql
+
+-- Create the 'user_db' database
+CREATE DATABASE user_db;
+
+-- Create the 'restaurant_db' database
+CREATE DATABASE restaurant_db;
